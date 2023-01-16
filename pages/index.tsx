bc153458ac96711fb856f8fcf4aa014a1d649019
Martin Clementz <martincclementz@gmail.com>
2023-01-16 19:25:41 +0100

    feat/header

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main >
+      <main>
           <p>
             Get started by editing&nbsp;
             <code>pages/index.tsx</code>
---
+++ utility/constants/routes.ts
@@ -0,0 +1,5 @@
+export const ROUTES = {
+    HISTORY: "/historie",
+    DALJER: "/daljer"
+}
+

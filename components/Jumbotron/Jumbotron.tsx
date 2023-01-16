4cd57aa75da7621a1f1de7b2cc279a2e7a35da30
MindChirp <frikk44@gmail.com>
2023-01-16 19:16:33 +0100

    Fixed gradient issues

--- components/Jumbotron/Jumbotron.tsx
+++ components/Jumbotron/Jumbotron.tsx
@@ -3,7 +3,10 @@ import styles from './Jumbotron.module.css'
 export const Jumbotron = ({...props}) => {
     return(
         <div className={styles.main}>
-            {props.children}
+            <div className={styles.content}>
+                {props.children}    
+            </div>
+            <div className={styles.gradientBackground}></div>
         </div>
     )
 }

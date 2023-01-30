c6ab3237b0f8160e3cb717b8d88576daf93a2123
MindChirp <frikk44@gmail.com>
2023-01-30 19:30:16 +0100

    Refactored jumbotron component

--- components/Jumbotron/Jumbotron.tsx
+++ components/Jumbotron/Jumbotron.tsx
@@ -1,5 +1,12 @@
 import styles from './Jumbotron.module.css'
 
+// Create a styled box component using the theme styling
+const StyledGradient = styled(Box)(({theme}) => ({
+    backgroundColor: theme.palette.primary.main,
+    background: `radial-gradient(circle, ${theme.palette.primary.main} 0%, ${theme.palette.primary.main} 35%, rgba(0, 0, 0, 1) 70%)`,
+}));
+
+// Create the Jumbotron container, using the styled gradient
 export const Jumbotron = ({...props}) => {
     return(
         <div className={styles.main}>

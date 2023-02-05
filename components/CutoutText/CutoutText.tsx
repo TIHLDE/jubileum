2933057bf9f82eec3480fc45c8989b16db12fcba
MindChirp <frikk44@gmail.com>
2023-02-05 06:25:04 +0100

    Added a alternate history page

---
+++ components/CutoutText/CutoutText.tsx
@@ -0,0 +1,49 @@
+import { isEqual } from 'date-fns';
+import styles from './CutoutText.module.css';
+import { useState, useEffect } from 'react';
+
+const CutoutText = ({ ...props }) => {
+  const [variant, setVariant] = useState(props.variant ?? 'left');
+  const [fontSize, setFontSize] = useState(props.fontSize ?? '1.5rem');
+  const [offset, setOffset] = useState(props.offset ?? 0);
+  const [display, setDisplay] = useState(props.display ?? 'none');
+  const [opactiy, setOpacity] = useState(1);
+
+  useEffect(() => {
+    if (!isEqual(props.offset, offset)) {
+      setOffset(props.offset);
+    }
+
+    if (!isEqual(props.display, display)) {
+      setDisplay(props.display);
+    }
+
+    // Update the opacity of the text
+    if (offset > 70) {
+      const calcOpac = (30 - (offset - 70)) / 30;
+      setOpacity(calcOpac > 0 ? calcOpac : 0);
+    }
+  }, [props.offset, props.display, offset, display]);
+
+  return (
+    <div
+      className={styles.wrapper}
+      style={{
+        textAlign: variant,
+        fontSize: fontSize,
+        display: display,
+        opacity: opactiy,
+      }}
+    >
+      <span className={styles.text}>{props.children}</span>
+      <div
+        className={styles.background}
+        style={{
+          transform: `translateY(-${offset}%)`,
+        }}
+      ></div>
+    </div>
+  );
+};
+
+export default CutoutText;
---
+++ components/ScrollyTelling/ScrollyTeller.tsx
@@ -0,0 +1,110 @@
+import { Chip } from '@mui/material';
+import CutoutText from '../CutoutText/CutoutText';
+import { Logo } from '../TihldeLogo/TihldeLogo';
+import styles from './ScrollyTeller.module.css';
+import { useState, useEffect } from 'react';
+
+export const ScrollyTeller = ({ ...props }) => {
+  const [percentage, setPercentage] = useState(0);
+
+  useEffect(() => {
+    // Add event listener for scrolling
+    window.addEventListener('scroll', handleScroll);
+
+    // Function to handle scrolling
+    function handleScroll() {
+      // Get the current scroll position
+      const position = window.pageYOffset;
+
+      // Get the height of the body
+      const bodyHeight = document.body.clientHeight;
+
+      const percentage = (position / (bodyHeight - 400)) * 100;
+
+      setPercentage(percentage > 100 ? 100 : percentage);
+    }
+
+    // Remove event listener on cleanup
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <>
+      <div className={styles.scrollyTeller}>
+        <div className={styles.scrollyTellerContent}>
+          {/* Display the TIHLDE logo to begin with */}
+
+          <CutoutText
+            variant={'center'}
+            fontSize={'10vw'}
+            offset={percentage * 10 > 100 ? 100 : percentage * 10}
+            display={percentage < 10 ? 'block' : 'none'}
+          >
+            20. Mars 1993
+          </CutoutText>
+
+          <CutoutText
+            variant={'center'}
+            fontSize={'10vw'}
+            offset={(percentage - 10) * 10 > 100 ? 100 : (percentage - 10) * 10}
+            display={percentage >= 10 && percentage < 20 ? 'block' : 'none'}
+          >
+            Tihlde Stiftes
+            <div
+              style={{
+                display: 'inline-block',
+                width: 'fit-content',
+              }}
+            >
+              <Chip label='Les mer' onClick={handleClick} />
+            </div>
+          </CutoutText>
+
+          <CutoutText
+            variant={'center'}
+            fontSize={'10vw'}
+            offset={(percentage - 20) * 5 > 100 ? 100 : (percentage - 20) * 5}
+            display={percentage >= 20 && percentage < 40 ? 'block' : 'none'}
+          >
+            Dette var en kjapp demo av en måte å formidle Tihldes historie på.
+          </CutoutText>
+
+          <CutoutText
+            variant={'center'}
+            fontSize={'10vw'}
+            offset={(percentage - 40) * 5 > 100 ? 100 : (percentage - 40) * 5}
+            display={percentage >= 40 && percentage < 60 ? 'block' : 'none'}
+          >
+            Den er ikke bugfri, men det er en start, i tilfelle vi vil gjøre det
+            på denne måten.
+          </CutoutText>
+
+          <CutoutText
+            variant={'center'}
+            fontSize={'10vw'}
+            offset={(percentage - 60) * 5 > 100 ? 100 : (percentage - 60) * 5}
+            display={percentage >= 60 && percentage < 80 ? 'block' : 'none'}
+          >
+            Dessuten kan man legge inn bilder, videoer, og andre ting, for å
+            formidle historien på en effektiv måte.
+          </CutoutText>
+
+          <CutoutText
+            variant={'center'}
+            fontSize={'10vw'}
+            offset={(percentage - 80) * 5 > 100 ? 100 : (percentage - 80) * 5}
+            display={percentage >= 80 && percentage < 100 ? 'block' : 'none'}
+          >
+            Til slutt kan vi evt. legge inn en vanlig tidslinje for å oppsummere
+            😎👌.
+          </CutoutText>
+        </div>
+      </div>
+      <div className={styles.bodyFiller}></div>
+    </>
+  );
+};
+
+function handleClick(): void {
+  alert('asdasdd');
+}

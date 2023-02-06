0ff8be1065b6177d053d5231a8ef4fe795762e2e
Martin Clementz <martincclementz@gmail.com>
2023-02-06 13:48:06 +0100

    feat(): added proper logo

--- components/Jumbotron/Jumbotron.tsx
+++ components/Jumbotron/Jumbotron.tsx
@@ -1,24 +1,27 @@
-import { useTheme } from '@emotion/react'
-import styles from './Jumbotron.module.css'
+import styles from "./Jumbotron.module.css";
 import { styled } from "@mui/material/styles";
-import { Box } from '@mui/material';
-import { Logo } from '../TihldeLogo/TihldeLogo';
+import { Box } from "@mui/material";
+import TihldeLogo from "../TihldeLogo/TihldeLogo";
 
 // Create a styled box component using the theme styling
-const StyledGradient = styled(Box)(({theme}) => ({
-    backgroundColor: theme.palette.primary.main,
-    background: `radial-gradient(circle, ${theme.palette.primary.main} 0%, ${theme.palette.primary.main} 35%, rgba(0, 0, 0, 1) 70%)`,
+const StyledGradient = styled(Box)(({ theme }) => ({
+  backgroundColor: theme.palette.primary.main,
+  background: `radial-gradient(circle, ${theme.palette.primary.main} 0%, ${theme.palette.primary.main} 35%, rgba(0, 0, 0, 1) 70%)`,
 }));
 
 // Create the Jumbotron container, using the styled gradient
-export const Jumbotron = ({...props}) => {
-    return(
-        <div className={styles.main}>
-            <div className={styles.content}>
-                <Logo />
-                {props.children}
-            </div>
-            <StyledGradient className={styles.gradientBackground}/>
-        </div>
-    )
-}
+export const Jumbotron = ({ ...props }) => {
+  return (
+    <div className={styles.main}>
+      <div className={styles.content}>
+        <TihldeLogo
+          logoColor="white"
+          size="large"
+          sx={{ height: 100, width: "auto" }}
+        />
+        {props.children}
+      </div>
+      <StyledGradient className={styles.gradientBackground} />
+    </div>
+  );
+};

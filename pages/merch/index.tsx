47c90be9bedbccb0b62553dad5c9f029d6e13dc1
eriskjel <eriskjel@stud.ntnu.no>
2023-02-13 17:35:30 +0100

    fixed images and removed unused items

--- pages/merch/index.tsx
+++ pages/merch/index.tsx
@@ -4,12 +4,8 @@ import styles from '../../components/Gallery/Gallery.module.css'
 const GalleryPage = () => {
     return (
         <div className={styles.wrapper_wrapper}>
-            <h1 style={{ textAlign: "center" }}>TIHLDE MERCH</h1>
+            <h1 className={styles.header}>TIHLDE MERCH</h1>
             <Gallery />
-            {/* <h3 style={{textAlign: "center"}}>UTKAST 1</h3>
-            <Gallery />
-            <h3 style={{textAlign: "center"}}>UTKAST 2</h3>
-            <GalleryV2 /> */}
         </div>
     )
 }

eb9e2f08b59ff37c9066ac32f30430b0f900f5d2
eriskjel <eriskjel@stud.ntnu.no>
2023-02-13 16:47:05 +0100

    deleted unused drafts

--- pages/merch/index.tsx
+++ pages/merch/index.tsx
@@ -1,12 +1,11 @@
 import { Gallery } from '../../components/Gallery/Gallery'
-import { GalleryV2 } from '../../components/Gallery/GalleryV2'
-import { GalleryV3 } from '../../components/Gallery/GalleryV3'
+import styles from '../../components/Gallery/Gallery.module.css'
 
 const GalleryPage = () => {
     return (
-        <div>
-            <h1 style={{textAlign: "center"}}>TIHLDE MERCH</h1>
-            <GalleryV3/>
+        <div className={styles.wrapper_wrapper}>
+            <h1 style={{ textAlign: "center" }}>TIHLDE MERCH</h1>
+            <Gallery />
             {/* <h3 style={{textAlign: "center"}}>UTKAST 1</h3>
             <Gallery />
             <h3 style={{textAlign: "center"}}>UTKAST 2</h3>

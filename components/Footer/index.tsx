778fee51b4c63f83b8bd224ff5be96dac74dd86d
eriskjel <eriskjel@stud.ntnu.no>
2023-02-20 17:17:05 +0100

    centered footer elements

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
+
 const Footer = () => {
   return (
     <Paper
@@ -17,12 +18,12 @@ const Footer = () => {
       }}
       square
     >
-      <Grid container spacing={5} sx={{ maxWidth: 1000 }} alignItems='center'>
-        <Grid item xs={12} md={4}>
+      <Grid container spacing={5} sx={{ alignItems: 'center', maxWidth: 1000 }}>
+        <Grid item xs={12} md={4} sx={{ display: "flex", justifyContent: "center" }}>
           <Typography>TIHLDE feirer 30 år! 🥳</Typography>
         </Grid>
         <Grid item xs={12} md={4}>
-          <Stack direction='row' gap={1} alignItems='center'>
+          <Stack direction='row' gap={1} alignItems='center' sx={{ justifyContent: "center" }}>
             <MuiLink
               component={Link}
               href='https://www.instagram.com/tihlde/'
@@ -39,13 +40,12 @@ const Footer = () => {
             />
           </Stack>
         </Grid>
-        <Grid item xs={12} md={4}>
-          Made with ❤️ by{' '}
+        <Grid item xs={12} md={4} sx={{ display: "flex", justifyContent: "center" }}>
+          Made with ❤️ by&nbsp; {'  '}
           <MuiLink
             sx={{ color: 'three.main' }}
             component={Link}
-            href='https://tihlde.org/wiki/tihlde/undergrupper/index/'
-          >
+            href='https://tihlde.org/wiki/tihlde/undergrupper/index/'>
             Index
           </MuiLink>
         </Grid>

1c16629f736b0023a9736d1adb8f9bc49784af05
MindChirp <frikk44@gmail.com>
2023-02-20 16:36:02 +0100

    Removed outdated merch

--- components/MerchBox/MerchItems.tsx
+++ components/MerchBox/MerchItems.tsx
@@ -6,24 +6,7 @@ type Merch = {
 };
 
 export const MerchItems: Array<Merch> = [
-  {
-    title: 'Blå crewneck m/ brodert jubileumslogo',
-    price: '450',
-    url: '/blåcrewMbrodert.png',
-    available: true,
-  },
-  {
-    title: 'Grå crewneck m/ limited edition logo',
-    price: '450',
-    url: '/GøyalVri.png',
-    available: true,
-  },
-  {
-    title: 'Lommelerke + fire shotteglass',
-    price: '300',
-    url: './lommelerke.png',
-    available: true,
-  },
+
   {
     title: 'Ølglass',
     price: '',

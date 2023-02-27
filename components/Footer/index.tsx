fbe29a073e17da934958f6c6ce4b83d3ab4ef7a1
MindChirp <frikk44@gmail.com>
2023-02-27 18:59:40 +0100

    Moved instagram logo to left of hyperlink

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -19,11 +19,27 @@ const Footer = () => {
       square
     >
       <Grid container spacing={5} sx={{ alignItems: 'center', maxWidth: 1000 }}>
-        <Grid item xs={12} md={4} sx={{ display: "flex", justifyContent: "center" }}>
+        <Grid
+          item
+          xs={12}
+          md={4}
+          sx={{ display: 'flex', justifyContent: 'center' }}
+        >
           <Typography>TIHLDE feirer 30 år! 🥳</Typography>
         </Grid>
         <Grid item xs={12} md={4}>
-          <Stack direction='row' gap={1} alignItems='center' sx={{ justifyContent: "center" }}>
+          <Stack
+            direction='row'
+            gap={1}
+            alignItems='center'
+            sx={{ justifyContent: 'center' }}
+          >
+            <Image
+              alt='instagram'
+              src='/icons/instagram.svg'
+              width={35}
+              height={35}
+            />
             <MuiLink
               component={Link}
               href='https://www.instagram.com/tihlde/'
@@ -32,20 +48,20 @@ const Footer = () => {
             >
               @Tihldejubileum
             </MuiLink>
-            <Image
-              alt='instagram'
-              src='/icons/instagram.svg'
-              width={35}
-              height={35}
-            />
           </Stack>
         </Grid>
-        <Grid item xs={12} md={4} sx={{ display: "flex", justifyContent: "center" }}>
+        <Grid
+          item
+          xs={12}
+          md={4}
+          sx={{ display: 'flex', justifyContent: 'center' }}
+        >
           Made with ❤️ by&nbsp; {'  '}
           <MuiLink
             sx={{ color: 'three.main' }}
             component={Link}
-            href='https://tihlde.org/wiki/tihlde/undergrupper/index/'>
+            href='https://tihlde.org/wiki/tihlde/undergrupper/index/'
+          >
             Index
           </MuiLink>
         </Grid>

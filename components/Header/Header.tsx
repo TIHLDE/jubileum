29177659f70ca062f57c41eef71400fe096da8b5
Tobias Rødahl Thingnes <tobbi.rt@gmail.com>
2023-02-27 19:22:53 +0100

    Added A-grade logo and replaced normie TIHLDE logo

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react"
 
 // Material
 import {
@@ -11,15 +11,15 @@ import {
   Link as MuiLink,
   Toolbar,
   useTheme,
-} from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
-import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+} from "@mui/material"
+import MenuIcon from "@mui/icons-material/Menu"
+import OpenInNewIcon from "@mui/icons-material/OpenInNew"
 
 // Assets
-import { ROUTES } from '../../utility/constants/routes';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import TihldeLogo from '../TihldeLogo/TihldeLogo';
+import { ROUTES } from "../../utility/constants/routes"
+import Link from "next/link"
+import { useRouter } from "next/router"
+import Logo from "../Logo"
 
 /*
   {
@@ -31,137 +31,139 @@ import TihldeLogo from '../TihldeLogo/TihldeLogo';
 const menu = [
   {
     link: ROUTES.MERCH,
-    label: 'Merch',
+    label: "Merch",
   },
   {
     link: ROUTES.TODDEL,
-    label: 'Tøddel',
+    label: "Tøddel",
   },
-];
+]
 
 export const Header: React.FunctionComponent = () => {
-  const [drawerOpen, setDrawerOpen] = useState(false);
-  const [tinted, setTinted] = useState(false);
-  const router = useRouter();
-  const theme = useTheme();
+  const [drawerOpen, setDrawerOpen] = useState(false)
+  const [tinted, setTinted] = useState(false)
+  const router = useRouter()
+  const theme = useTheme()
 
   const toggleDrawer =
     (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
       if (
-        event.type === 'keydown' &&
-        ((event as React.KeyboardEvent).key === 'Tab' ||
-          (event as React.KeyboardEvent).key === 'Shift')
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
       ) {
-        return;
+        return
       }
-      setDrawerOpen(open);
-    };
+      setDrawerOpen(open)
+    }
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll)
 
     function handleScroll() {
-      const position = window.scrollY;
+      const position = window.scrollY
 
       if (position > 5 && !tinted) {
         // Set the background color of the header to a semi-translucent color
-        setTinted(true);
+        setTinted(true)
       } else if (position <= 5 && tinted) {
-        setTinted(false);
+        setTinted(false)
       }
     }
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [tinted]);
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [tinted])
 
   return (
     <React.Fragment>
       <AppBar
-        position='sticky'
+        position="sticky"
         elevation={0}
         style={{
-          transition: 'background-color 0.25s',
-          backdropFilter: 'blur(5px)',
-          WebkitBackdropFilter: 'blur(5px)',
-          position: 'fixed',
+          transition: "background-color 0.25s",
+          backdropFilter: "blur(5px)",
+          WebkitBackdropFilter: "blur(5px)",
+          position: "fixed",
         }}
-        sx={{ backgroundColor: tinted ? 'rgba(0,0,0,0.3)' : 'rgba(0,0,0,0)' }}
+        sx={{ backgroundColor: tinted ? "rgba(0,0,0,0.3)" : "rgba(0,0,0,0)" }}
       >
         <Toolbar disableGutters>
           <Container
             style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              alignItems: 'center',
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
             }}
-            maxWidth='xl'
+            maxWidth="xl"
           >
-            <MuiLink component={Link} href='/' onClick={toggleDrawer(false)}>
-              <TihldeLogo
-                size='large'
-                logoColor='white'
-                sx={{ height: 30, width: 'auto' }}
+            <MuiLink component={Link} href="/" onClick={toggleDrawer(false)}>
+              <Logo
+                sx={{
+                  width: 120,
+                  color: "white",
+                  marginTop: 1,
+                }}
               />
             </MuiLink>
             <Box
-              sx={{ display: { xs: 'none', md: 'flex' } }}
-              flexDirection='row'
-              justifyContent='center'
+              sx={{ display: { xs: "none", md: "flex" } }}
+              flexDirection="row"
+              justifyContent="center"
             >
               {menu.map((item, i) => (
                 <Button
-                  target={item.label == 'Tøddel' ? '_blank' : undefined}
+                  target={item.label == "Tøddel" ? "_blank" : undefined}
                   key={i}
                   component={Link}
                   href={item.link}
                   style={{
-                    textAlign: 'center',
-                    color: 'white',
+                    textAlign: "center",
+                    color: "white",
                     ...(router.pathname == item.link
                       ? {
                           fontWeight: 600,
                           borderBottomLeftRadius: 0,
                           borderBottomRightRadius: 0,
-                          borderBottom: '2px solid white',
+                          borderBottom: "2px solid white",
                         }
                       : {}),
                   }}
                 >
                   {item.label}
-                  {item.label == 'Tøddel' && (
-                    <OpenInNewIcon sx={{ ml: 1 }} fontSize='small' />
+                  {item.label == "Tøddel" && (
+                    <OpenInNewIcon sx={{ ml: 1 }} fontSize="small" />
                   )}
                 </Button>
               ))}
             </Box>
             <Box
-              sx={{ display: { xs: 'none', md: 'flex' } }}
-              flexDirection='row'
-              justifyContent='end'
-              alignItems='center'
+              sx={{ display: { xs: "none", md: "flex" } }}
+              flexDirection="row"
+              justifyContent="end"
+              alignItems="center"
             >
               <Button
-                variant='outlined'
-                href='https://tihlde.org/'
-                target='_blank'
-                color='info'
+                variant="outlined"
+                href="https://tihlde.org/"
+                target="_blank"
+                color="info"
               >
                 tihlde.org
-                <OpenInNewIcon sx={{ ml: 1 }} fontSize='small' />
+                <OpenInNewIcon sx={{ ml: 1 }} fontSize="small" />
               </Button>
             </Box>
             <Box
-              sx={{ display: { xs: 'flex', md: 'none' } }}
-              flexDirection='row'
-              justifyContent='end'
+              sx={{ display: { xs: "flex", md: "none" } }}
+              flexDirection="row"
+              justifyContent="end"
             >
               <IconButton
-                size='large'
+                size="large"
                 onClick={toggleDrawer(true)}
-                color='info'
+                color="info"
                 sx={{ mr: -1.5 }}
               >
                 <MenuIcon />
@@ -170,26 +172,26 @@ export const Header: React.FunctionComponent = () => {
           </Container>
         </Toolbar>
         <Drawer
-          variant='temporary'
-          anchor='right'
+          variant="temporary"
+          anchor="right"
           open={drawerOpen}
           onClose={toggleDrawer(false)}
-          PaperProps={{ sx: { backgroundColor: 'black' } }}
+          PaperProps={{ sx: { backgroundColor: "black" } }}
         >
           <Container sx={{ width: 250, p: 2 }}>
             <Box
-              flexDirection='column'
-              sx={{ display: { xs: 'flex', md: 'none' } }}
-              alignItems='end'
+              flexDirection="column"
+              sx={{ display: { xs: "flex", md: "none" } }}
+              alignItems="end"
               style={{
-                transition: 'height 0.25s',
-                overflow: 'hidden',
+                transition: "height 0.25s",
+                overflow: "hidden",
               }}
             >
               {menu.map((item, i) => (
                 <Button
                   key={i}
-                  color='info'
+                  color="info"
                   onClick={toggleDrawer(false)}
                   component={Link}
                   href={item.link}
@@ -197,10 +199,10 @@ export const Header: React.FunctionComponent = () => {
                     router.pathname == item.link
                       ? {
                           fontWeight: 600,
-                          color: 'white',
+                          color: "white",
                           borderBottomRightRadius: 0,
                           borderTopRightRadius: 0,
-                          borderRight: '2px solid white',
+                          borderRight: "2px solid white",
                         }
                       : {}
                   }
@@ -209,18 +211,18 @@ export const Header: React.FunctionComponent = () => {
                 </Button>
               ))}
               <Button
-                href='https://tihlde.org/'
-                target='_blank'
-                color='info'
+                href="https://tihlde.org/"
+                target="_blank"
+                color="info"
                 onClick={toggleDrawer(false)}
               >
                 tihlde.org
-                <OpenInNewIcon sx={{ ml: 1 }} fontSize='small' />
+                <OpenInNewIcon sx={{ ml: 1 }} fontSize="small" />
               </Button>
             </Box>
           </Container>
         </Drawer>
       </AppBar>
     </React.Fragment>
-  );
-};
+  )
+}

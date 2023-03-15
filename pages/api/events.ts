d2bc611613c33d16b204494ad2755038eaf6b5a7
Martin Clementz <martcl@stud.ntnu.no>
2023-03-15 23:41:58 +0100

    feat(noSSR): client henter event data fra api route

---
+++ pages/api/events.ts
@@ -0,0 +1,34 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+    events: any[];
+};
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    const urlsUnfiltered = await fetch(
+        "https://api.tihlde.org/events/?&organizer=jubkom"
+    )
+        .then((resp) => resp.json())
+        .then((data) => data.results.map((event: { id: any }) => event.id));
+
+    //removing url element "489" for some reason?
+    const urls = urlsUnfiltered.filter((url: any) => url !== 489);
+
+    const events = await Promise.all(
+        urls.map((url: any) =>
+            fetch(`https://api.tihlde.org/events/${url}/`).then((resp) => resp.json())
+        )
+    );
+    res
+        .status(200)
+        .setHeader(
+            "cache-control",
+            "public, s-maxage=1200, stale-while-revalidate=600"
+        )
+        .setHeader("content-type", "application/json")
+        .json({ events: events });
+}
